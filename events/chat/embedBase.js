7b981b9f37dcdd588296b3cfa6d7ed100573aadf
fehzitu <fehzitujsdev@gmail.com>
2026-02-26 22:02:17 -0300

    embed base command

---
+++ events/chat/embedBase.js
@@ -0,0 +1,44 @@
+// discord implements
+const Discord = require('discord.js');
+
+// node file system
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'complements/embed.json');
+
+module.exports = {
+    name: 'messageCreate',
+    async execute(message) {
+        // check if an bot has send the message
+        if (message.author.bot) return;
+
+        // get the content
+        const content = message.content.toLowerCase();
+
+        // reading the file in real time
+        const rawData = fs.readFileSync(filePath, 'utf8');
+        const data = JSON.parse(rawData);
+
+        // create an embed
+        const embed = new Discord.EmbedBuilder()
+            .setColor('Random')
+            .setAuthor({
+                iconURL: `${message.author.displayAvatarURL()}`,
+                name: `@${message.author.username}`
+            })
+            .setTitle(data.title)
+            .setThumbnail(`${message.author.displayAvatarURL()}`)
+            .addFields(data.field)
+            .setTimestamp()
+            .setFooter({
+                text: 'Atualizado'
+            });
+
+        // checking the regex
+        if (content == 'embed') {
+            const response = await message.reply({
+                embeds: [embed]
+            });
+        };
+    }
+};
