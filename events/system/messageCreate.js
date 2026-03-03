8f12cc50bc3c04aeac72286cfa128ddf505a0138
fehzitu <fehzitujsdev@gmail.com>
2026-03-03 14:15:44 -0300

    fix event limit

--- events/system/messageCreate.js
+++ events/system/messageCreate.js
@@ -12,5 +12,26 @@ module.exports = {
 
         // log
         console.log(`[${new Date().toLocaleTimeString()}] ${message.author.tag} -> ${guildName} #${channelName}: ${message.content}`);
+
+		// commands prefix
+        const prefix = "k.";
+
+		// set lower case
+        if (!message.content.toLowerCase().startsWith(prefix)) return;
+
+		// get args from message content
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+        const commandName = args.shift().toLowerCase();
+
+		// get the command by the name
+        const command = message.client.commands.get(commandName);
+        if (!command) return;
+
+		// handle error
+        try {
+            await command.execute(message, args);
+        } catch (error) {
+            console.error(error);
+        }
     }
 };
