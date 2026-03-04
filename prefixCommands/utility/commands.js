8f8fd0111ecaf04a25bea26f90438d6fda001a26
fehzitu <fehzitujsdev@gmail.com>
2026-03-04 06:55:17 -0300

    refactoring the commands command for the new system structure

--- prefixCommands/utility/commands.js
+++ prefixCommands/utility/commands.js
@@ -7,14 +7,11 @@ const path = require('path');
 const filePath = path.join(__dirname, 'complements/commands.json');
 
 module.exports = {
-    name: 'messageCreate',
-    once: false,
+    // "name" will receive the value that will be the chat message that the bot captures as a command
+    name: 'comandos',
     async execute(message) {
         // check if an bot has send the message
         if (message.author.bot) return;
-
-        // catch the message content and set to lower case
-        const content = message.content.toLowerCase();
 
         // reading the file in real time
         const rawData = fs.readFileSync(filePath, 'utf8');
@@ -34,11 +31,9 @@ module.exports = {
                 text: 'Atualizado'
             });
 
-        // check if the message has "k.comandos"
-        if (content == 'k.comandos') {
-            const response = await message.reply({
-                embeds: [embed]
-            });
-        };
+        // response
+        await message.reply({
+            embeds: [embed]
+        });
     }
 };
