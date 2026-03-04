d7a59e3e1ec1f7f719782ad68dfacdf33f4f51d8
fehzitu <fehzitujsdev@gmail.com>
2026-03-04 15:18:03 -0300

    finish kiss command (maybe)

--- prefixCommands/chat/kiss.js
+++ prefixCommands/chat/kiss.js
@@ -1,11 +1,6 @@
 // discord implements
 const Discord = require('discord.js');
 
-// node file system
-const fs = require('fs');
-const path = require('path');
-const filePath = path.join(__dirname, 'complements/kiss.json');
-
 module.exports = {
     // "name" will receive the value that will be the chat message that the bot captures as a command
     name: 'beijar',
@@ -19,18 +14,11 @@ module.exports = {
         // get the first mentioned user id in string (without <@ >)
         const firstMentionedUser = message.mentions.users.first();
 
-        // reading the file in real time
-        const rawData = fs.readFileSync(filePath, 'utf8');
-        const data = JSON.parse(rawData);
-
-        // variable to save the api img url
-        let imgUrl = '';
-
         // function to get the api img
         const getImg = async (args) => {
             // get the image from an external API
             const fetchApi = await fetch(`https://api.waifu.pics/sfw/${args}`);
-            imgUrl = await fetchApi.json();
+            return await fetchApi.json();
         };
 
         // create an errorEmbed
@@ -40,7 +28,16 @@ module.exports = {
                 iconURL: `${message.author.displayAvatarURL()}`,
                 name: `@${message.author.username}`
             })
-            .addFields(data.error)
+            .addFields([
+                {
+                    "name": "🔴 **Uso incorreto do comando**!",
+                    "value": "(Faltou alguma marcação ou mais de 1 usuário foi marcado!)"
+                },
+                {
+                    "name": "🟢 **Uso correto**:",
+                    "value": "k.beijar @[usuário]"
+                }
+            ])
             .setTimestamp()
             .setFooter({
                 text: 'Atualizado'
@@ -58,23 +55,25 @@ module.exports = {
                 text: 'Atualizado'
             });
 
+        let img = message.client.user.displayAvatarURL();
+
         // set the embed to a mentioned user
         if (content.length == 2 && firstMentionedUser) {
             // check if the user use kiss on him self
             if (message.author.id == firstMentionedUser.id) {
-                // call the img function
-                await getImg('cringe');
+                // get the img link
+                img = await getImg('cringe');
 
                 // set the title of embed with mentioned username
                 successEmbed.setDescription(`🤔 **<@${message.author.id}> se ama ao ponto de se beijar?**❓`);
-                await successEmbed.setImage(imgUrl.url);
+                successEmbed.setImage(img.url);
             } else {
-                // call the img function
-                await getImg('kiss');
+                // get the img link
+                img = await getImg('kiss');
 
                 // set the title of embed with mentioned username
                 successEmbed.setDescription(`😳 **<@${message.author.id}> beijou <@${firstMentionedUser.id}>**❗`);
-                await successEmbed.setImage(imgUrl.url);
+                successEmbed.setImage(img.url);
             };
 
             // response
